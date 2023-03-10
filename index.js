41491940b9f163db3cf51f74639da55fc0541152
r480github <119364101+r480github@users.noreply.github.com>
2023-03-10 13:29:35 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var password = "jpermcool"
+function passcheck() {
+
+    if(document.getElementById('pass1').value !=password) {
+    alert('AccesDenied')    
+        return false;
+    }
+    
+        if(document.getElementById('pass1').value ==password) {
+    }
+
+}
